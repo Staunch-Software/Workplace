bf9e0af4a27d00e24cb6eff28fbcff89da3fb0fb
Staunch-Software <software@staunchtec.com>
2026-03-10 14:11:15 +0530

    BaseURL change

--- workplace-app/src/modules/drs/api/axiosDrs.js
+++ workplace-app/src/modules/drs/api/axiosDrs.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiDrs = axios.create({
   // baseURL: 'http://localhost:8001/api/v1', // drs-backend
   // baseURL: 'http://52.172.91.85:8001/api/v1',
-  baseURL: "/drs",
+  baseURL: "/drs/api/v1",
   headers: { 'Content-Type': 'application/json' },
 });
 
