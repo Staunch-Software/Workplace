1d3b29b998b9c9da00c593fc3c9fb93f809250d7
Staunch-Software <software@staunchtec.com>
2026-03-10 12:51:08 +0530

    BaseURL change

--- workplace-app/src/api/axios.js
+++ workplace-app/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'http://localhost:8003/api/v1', // workplace-backend
   // baseURL: 'http://52.172.91.85:8003/api/v1',
-  baseURL: "/api",
+  baseURL: "/api/v1",
   headers: { 'Content-Type': 'application/json' },
 });
 
