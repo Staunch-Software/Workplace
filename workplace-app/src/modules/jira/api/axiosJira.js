f306fd11734e0ae345b0e4bfbd4b807a39b3f954
Sujil Kumar <sujilkumar3003@gmail.com>
2026-05-04 15:21:19 +0530

    Updated backend sync, frontend UI, and API changes

--- workplace-app/src/modules/jira/api/axiosJira.js
+++ workplace-app/src/modules/jira/api/axiosJira.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { handleExpiredSession } from '../../../utils/authGuard';
 
 const axiosJira = axios.create({
-  baseURL: 'http://localhost:8004',
-  // baseURL: '/jira/api',
+  // baseURL: 'http://localhost:8004',
+  baseURL: '/jira/api',
 });
 
 axiosJira.interceptors.request.use((config) => {
